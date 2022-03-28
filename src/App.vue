13a8efdaf7936801456701a71b3608c1519ddb03
Massimo1204 <xiangmassimo@gmail.com>
2022-03-28 17:53:51 +0200

    base

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <mainHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import mainHeader from "./components/header.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    mainHeader,
   },
 };
 </script>
---
+++ src/components/header.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <img src="../assets/img/dc-logo.png" alt="dc logo" />
+      </div>
+      <div>
+        <ul></ul>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainHeader",
+  data: function () {
+    return {
+      headerNav: [
+        {
+          text: "characters",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "comics",
+          url: "#",
+          state: true,
+        },
+        {
+          text: "movies",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "tv",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "games",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "collectibles",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "videos",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "fans",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "news",
+          url: "#",
+          state: false,
+        },
+        {
+          text: "shop",
+          url: "#",
+          state: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
